26884082a82e066c88d58a53029fb635bb024307
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2014-03-28 18:09:40 -0300

    src/main/java/com/codeslashers/Chapter2

---
+++ src/main/java/com/codeslashers/Chapter2.java
@@ -0,0 +1,32 @@
+package com.codeslashers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Chapter2 {
+	
+	public static void main(String... args) {
+		
+		User user1 = new User("Paulo Silveira", 150);
+		User user2 = new User("Rodrigo Turini", 120);
+		User user3 = new User("Guilherme Silveira", 190);
+
+		List<User> users = Arrays.asList(user1, user2, user3);
+
+		for (User user : users) {
+			System.out.println(user.getName());
+		}
+		
+		users.forEach(new Consumer<User>() {
+			public void accept(User user) {
+				System.out.println(user.getName());
+			}
+		});
+		
+		users.forEach(u -> System.out.println(u.getName()));
+		
+		users.forEach(u -> u.becameModerator());
+		
+	}
+}
