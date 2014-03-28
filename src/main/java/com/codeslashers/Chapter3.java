c1c2384f05ecd42302dcd04a031b1f3cfe4bcacb
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2014-03-28 18:18:50 -0300

    src/main/java/com/codeslashers/Chapter3

---
+++ src/main/java/com/codeslashers/Chapter3.java
@@ -0,0 +1,45 @@
+package com.codeslashers;
+
+class Chapter3 {
+
+	public static void main(String[] args) {
+
+		Runnable r1 = new Runnable(){
+			public void run(){
+				for (int i = 0; i <= 1000; i++) {
+					System.out.println(i);
+				}
+			}
+		};
+		new Thread(r1).start();
+		
+		Runnable r2 = () -> {
+			for (int i = 0; i <= 1000; i++) {
+				System.out.println(i);
+			}
+		};
+		new Thread(r2).start();
+		
+		new Thread(() -> {
+			for (int i = 0; i <= 1000; i++) {
+				System.out.println(i);
+			}
+		}).start();
+		
+		Validator<String> zipCodeValidator = new Validator<String>() {
+			public boolean validates(String value) {
+				return value.matches("[0-9]{5}-[0-9]{3}");
+			}	
+		};
+		
+		Validator<String> zipCodeValidatorAsLambda = 
+				valor -> valor.matches("[0-9]{5}-[0-9]{3}");
+		
+		Runnable o = () -> {
+			System.out.println("Which lambda am I?");
+		};
+
+		System.out.println(o);
+		System.out.println(o.getClass());
+	}
+}
